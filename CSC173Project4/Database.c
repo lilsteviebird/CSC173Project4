c07760a9fb0f5c8f5aecf15fe210befb9193b5ba
lilsteviebird <stevenli880@gmail.com>
2020-11-16 17:42:16 -0500

    finished insert for all tables

--- CSC173Project4/Database.c
+++ CSC173Project4/Database.c
@@ -9,6 +9,7 @@
 #include "Database.h"
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 
 Database new_Database(int size){
@@ -18,7 +19,11 @@ Database new_Database(int size){
     }
     this->size = size;
     for(int i = 0; i < size; i++){
-        this->buckets[i] = new_CSGLIST();
+        this->csgBuckets[i] = new_CSGLIST();
+        this->cdhBuckets[i] = new_CDHLIST();
+        this->cpBuckets[i] = new_CPLIST();
+        this->crBuckets[i] = new_CRLIST();
+        this->snapBuckets[i] = new_SNAPLIST();
     }
     this->count = 0;
     return this;
@@ -29,14 +34,56 @@ static int hashId_Method(Database this, int idGiven){
     return index;
 }
 
+static int hashCourse_Method(Database this, char* courseGiven){
+    int index = 0;
+    for(int i = 0; i < strlen(courseGiven); i++){
+        index = index * (int) courseGiven[i];
+    }
+    index = index % this->size;
+    return index;
+}
+
 void insert_CSGDatabase(char* courseGiven, int idGiven, char* gradeGiven, Database this){
     int index = hashId_Method(this, idGiven);
-    add_CSGLIST(this->buckets[index], courseGiven, idGiven, gradeGiven);
+    add_CSGLIST(this->csgBuckets[index], courseGiven, idGiven, gradeGiven);
+}
+void insert_SNAPDatabase(int idGiven, char* nameGiven, char* addressGiven, char* phoneGiven, Database this){
+    int index = hashId_Method(this, idGiven);
+    add_SNAPLIST(this->snapBuckets[index], idGiven, nameGiven, addressGiven, phoneGiven);
+}
+void insert_CDHDatabase(char* courseGiven, char* dayGiven, int hourGiven, Database this){
+    int index = hashCourse_Method(this, courseGiven);
+    add_CDHLIST(this->cdhBuckets[index], courseGiven, dayGiven, hourGiven);
+}
+void insert_CRDatabase(char* courseGiven, char* roomGiven, Database this){
+    int index = hashCourse_Method(this, courseGiven);
+    add_CRLIST(this->crBuckets[index], courseGiven, roomGiven);
+}
+void insert_CPDatabase(char* courseGiven, char* prereqGiven, Database this){
+    int index = hashCourse_Method(this, courseGiven);
+    add_CPLIST(this->cpBuckets[index], courseGiven, prereqGiven);
 }
 
 void print_Database(Database this){
+    printf("Printing CSG database...\n");
     for(int i = 0; i< this->size; i++){
-        print_CSGLIST(this->buckets[i]);
+        print_CSGLIST(this->csgBuckets[i]);
+    }
+    printf("Printing CDH database...\n");
+    for(int i = 0; i< this->size; i++){
+        print_CDHLIST(this->cdhBuckets[i]);
+    }
+    printf("Printing CR database...\n");
+    for(int i = 0; i< this->size; i++){
+        print_CRLIST(this->crBuckets[i]);
+    }
+    printf("Printing CP database...\n");
+    for(int i = 0; i< this->size; i++){
+        print_CPLIST(this->cpBuckets[i]);
+    }
+    printf("Printing SNAP database...\n");
+    for(int i = 0; i< this->size; i++){
+        print_SNAPLIST(this->snapBuckets[i]);
     }
 }
 
--- CSC173Project4/Database.h
+++ CSC173Project4/Database.h
@@ -12,16 +12,29 @@
 #include <stdio.h>
 
 #include "CSGLIST.h"
+#include "CPLIST.h"
+#include "SNAPLIST.h"
+#include "CDHLIST.h"
+#include "CRLIST.h"
 
 typedef struct Database* Database;
 
 struct Database{
     int size;
     int count;
-    CSGLIST* buckets[5];
+    CSGLIST* csgBuckets[5];
+    SNAPLIST* snapBuckets[5];
+    CDHLIST* cdhBuckets[5];
+    CRLIST* crBuckets[5];
+    CPLIST* cpBuckets[5];
+    
 };
 
 extern Database new_Database(int size);
 extern void insert_CSGDatabase(char* courseGiven, int idGiven, char* gradeGiven, Database this);
+extern void insert_SNAPDatabase(int idGiven, char* nameGiven, char* addressGiven, char* phoneGiven, Database this);
+extern void insert_CDHDatabase(char* courseGiven, char* dayGiven, int hourGiven, Database this);
+extern void insert_CRDatabase(char* courseGiven, char* roomGiven, Database this);
+void insert_CPDatabase(char* courseGiven, char* prereqGiven, Database this);
 extern void print_Database(Database this);
 #endif /* Database_h */
