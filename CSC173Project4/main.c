e1b706c52561c9a93e8de5a57b3c15eb1a9dd883
lilsteviebird <stevenli880@gmail.com>
2020-11-15 19:26:15 -0500

    Initial Commit

---
+++ CSC173Project4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSC173Project4
+//
+//  Created by sli19 on 11/15/20.
+//  Copyright © 2020 sli19. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
